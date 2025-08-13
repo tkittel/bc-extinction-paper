7a6822da1f8825d8e67b160fe0d859999e8888c4
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-08-13 22:13:07 +0200

    Add phi0(sigma*r) and phipi(sigma*r) implementations

--- src/bcextn/cli_test.py
+++ src/bcextn/cli_test.py
@@ -1,4 +1,8 @@
 def main():
     from .eval_fofeta import test_f_of_eta
     test_f_of_eta()
+    from .eval_phi0 import test_phi0
+    test_phi0()
+    from .eval_phipi import test_phipi
+    test_phipi()
     print("All ok")
