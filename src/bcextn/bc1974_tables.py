e291af8463481272cc9bb5fdf2dac9b1af1a8ca6
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-08-15 13:19:06 +0200

    Add BC1974 Table1 data

---
+++ src/bcextn/bc1974_tables.py
@@ -0,0 +1,37 @@
+
+_cache_table1 = [None]
+def table1():
+    if _cache_table1[0] is None:
+        from .data import load_json_data
+        import numpy as np
+        def array( x ):
+            return np.asarray( x, dtype=float )
+        all_values = []
+        raw = load_json_data('bc1974_table1.json')
+        assert set(raw.keys()) == set(['xvals','data'])
+        xvals = array( raw['xvals'] )
+        #sinth2ypvals = {}
+        sinthvals = []
+        x_sinth_to_yp = {}
+        for e in raw['data']:
+            assert set(e.keys()) == set(['sinth','yp*1e4'])
+            sinth = float(e['sinth'])
+            sinthvals.append( sinth )
+            ypvals = array( [ yp*1e-4 for yp in e['yp*1e4'] ] )
+            assert len(ypvals) == len(xvals)
+            #sinth2ypvals[sinth] = ypvals
+            for x, yp in zip(xvals,ypvals):
+                all_values.append( ( x, sinth, yp ) )
+                key = ( float(x), sinth )
+                assert key not in x_sinth_to_yp
+                x_sinth_to_yp[key] = float(yp)
+
+        _cache_table1[0] = { 'xvals' : xvals,
+                             'sinth' : array(sinthvals),
+                             'x_sinth_to_yp' : x_sinth_to_yp,
+                             #'sinth_2_yp' : sinth2ypvals,
+                             'x_sinth_yp_list' : array( sorted(all_values) ) }
+
+    return _cache_table1[0]
+
+
---
+++ src/bcextn/cli_table1.py
@@ -0,0 +1,5 @@
+from .bc1974_tables import table1
+
+def main():
+    import pprint
+    pprint.pprint(table1())
