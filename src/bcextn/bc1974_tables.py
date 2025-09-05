6e83267d17ae37453655bbe13b8c7ab430ef9870
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-09-05 15:11:10 +0200

    Add JSON versions of Table 3 and 4 (via OCR + manual checks)

--- src/bcextn/bc1974_tables.py
+++ src/bcextn/bc1974_tables.py
@@ -1,37 +1,52 @@
 
+def _do_load(fn, yp_or_ys):
+    assert yp_or_ys in ('yp','ys')
+    from .data import load_json_data
+    import numpy as np
+    def array( x ):
+        return np.asarray( x, dtype=float )
+    all_values = []
+    raw = load_json_data(fn)
+    assert set(raw.keys()) == set(['xvals','data'])
+    xvals = array( raw['xvals'] )
+    sinthvals = []
+    x_sinth_to_yps = {}
+    for e in raw['data']:
+        #print( set(e.keys()))
+        #print(set(['sinth',f'{yp_or_ys}*1e4']))
+        assert set(e.keys()) == set(['sinth',f'{yp_or_ys}*1e4'])
+        sinth = float(e['sinth'])
+        sinthvals.append( sinth )
+        ypsvals = array( [ yps*1e-4 for yps in e[f'{yp_or_ys}*1e4'] ] )
+        assert len(ypsvals) == len(xvals)
+        for x, yps in zip(xvals,ypsvals):
+            all_values.append( ( x, sinth, yps ) )
+            key = ( float(x), sinth )
+            assert key not in x_sinth_to_yps
+            x_sinth_to_yps[key] = float(yps)
+
+    #FIXME: 'yp' keys to yy?
+    return { 'xvals' : xvals,
+             'sinthvals' : array(sinthvals),
+             'x_sinth_to_yp' : x_sinth_to_yps,
+             'x_sinth_yp_list' : array( sorted(all_values) ) }
+
 _cache_table1 = [None]
 def table1():
     if _cache_table1[0] is None:
-        from .data import load_json_data
-        import numpy as np
-        def array( x ):
-            return np.asarray( x, dtype=float )
-        all_values = []
-        raw = load_json_data('bc1974_table1.json')
-        assert set(raw.keys()) == set(['xvals','data'])
-        xvals = array( raw['xvals'] )
-        #sinth2ypvals = {}
-        sinthvals = []
-        x_sinth_to_yp = {}
-        for e in raw['data']:
-            assert set(e.keys()) == set(['sinth','yp*1e4'])
-            sinth = float(e['sinth'])
-            sinthvals.append( sinth )
-            ypvals = array( [ yp*1e-4 for yp in e['yp*1e4'] ] )
-            assert len(ypvals) == len(xvals)
-            #sinth2ypvals[sinth] = ypvals
-            for x, yp in zip(xvals,ypvals):
-                all_values.append( ( x, sinth, yp ) )
-                key = ( float(x), sinth )
-                assert key not in x_sinth_to_yp
-                x_sinth_to_yp[key] = float(yp)
-
-        _cache_table1[0] = { 'xvals' : xvals,
-                             'sinthvals' : array(sinthvals),
-                             'x_sinth_to_yp' : x_sinth_to_yp,
-                             #'sinth_2_yp' : sinth2ypvals,
-                             'x_sinth_yp_list' : array( sorted(all_values) ) }
-
+        _cache_table1[0] = _do_load('bc1974_table1.json','yp')
     return _cache_table1[0]
+
+_cache_table3 = [None]
+def table3():
+    if _cache_table3[0] is None:
+        _cache_table3[0] = _do_load('bc1974_table3.json','ys')
+    return _cache_table3[0]
+
+_cache_table4 = [None]
+def table4():
+    if _cache_table4[0] is None:
+        _cache_table4[0] = _do_load('bc1974_table4.json','ys')
+    return _cache_table4[0]
 
 
