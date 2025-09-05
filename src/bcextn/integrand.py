8e999a7d89e16b6f4969a95c1f3d6f62edd92403
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-09-05 19:22:15 +0200

    Refactor code for creating integrand fct

---
+++ src/bcextn/integrand.py
@@ -0,0 +1,26 @@
+def create_integrand_fct_eq36( f_of_eta_fct, theta_degree, x, xscale = 1 ):
+    from .mpmath import mp, mpf
+    from .eval_phipi import PhiPi
+    from .eval_phi0 import Phi0
+    f = f_of_eta_fct
+    phi0 = Phi0()
+    phipi = PhiPi()
+    x = mpf(x)
+    sinth = mp.sin(mpf(theta_degree) * mp.pi/mpf(180) )
+    sqrtsinth = mp.sqrt( sinth )
+    sinth_to_3div2 = sinth * sqrtsinth
+    if not sinth:
+        phi = phi0
+    elif sinth == mpf(1):
+        phi = phipi
+    else:
+        def phi( sr ):
+            #BC 1974 eq. 34 ( sr = sigma*r )
+            assert sr >= 0.0
+            u = sr * sqrtsinth
+            return phi0( sr ) + sinth_to_3div2 * ( phipi(u) - phi0(u) )
+    xscale = mpf(xscale)
+    def integrand( eta ):
+        fval = f(eta)
+        return fval * phi( xscale * x * fval )
+    return integrand
