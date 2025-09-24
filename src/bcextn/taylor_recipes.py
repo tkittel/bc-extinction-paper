c1f30b43cd8062569aafef9ca12795b732643643
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-09-24 13:03:57 +0200

    Add module with 5th order taylor coefficients for y0 and ydelta

--- src/bcextn/taylor_recipes.py
+++ src/bcextn/taylor_recipes.py
@@ -1,22 +1,61 @@
-#Taylor expansions, for user recipes (FIXME: Unclear if we will use this ultimately):
+def taylor_y0_coeffs( mode ):
+    from .mpmath import mp, mpf
+    #5th order taylor coefficients for y(x,theta=0)
+    if mode == 'primary':
+        return [ mpf(1),
+                 mpf("-33/35"),
+                 mpf("4738/5775"),
+                 mpf("-54120476/91216125"),
+                 mpf("350769113251/962451740250"),
+                 mpf("-276478446363113/1423053644512500") ]
+    if mode == 'scndgauss':
+        return [ mpf(1),
+                 mp.sqrt(2)*mpf("-3/4"),
+                 mp.sqrt(3)*mpf("8/15"),
+                 mpf("-2/3"),
+                 mp.sqrt(5)*mpf("32/175"),
+                 mp.sqrt(3)*mp.sqrt(2)*mpf("-4/45") ]
+    if mode == 'scndlorentz':
+        return [ mpf(1),
+                 mpf("-1"),
+                 mpf("16/15"),
+                 mpf("-80/81"),
+                 mpf("64/81"),
+                 mpf("-224/405") ]
+    if mode == 'scndfresnel':
+        return [ mpf(1),
+                 mpf("-1"),
+                 mpf("22/25"),
+                 mpf("-604/945"),
+                 mpf("15619/39690"),
+                 mpf("-655177/3118500") ]
+    assert False
 
-import math
-
-def taylor_lowx_scndgauss( x, sintheta ):
-    u = 2*x
-    s_sqrt = math.sqrt(sintheta)
-    s2 = sintheta*sintheta
-    s3, s4 = sintheta*s2, s2*s2
-    s5 = s2*s3
-    sqrt2, sqrt3 = 1.4142135623730951, 1.7320508075688772
-    sqrt5, sqrt7 = 2.23606797749979, 2.6457513110645907
-    c0 = 1.0
-    c1 = sqrt2*(-3./8.)
-    c2 = sqrt3*(2./15.)+sqrt3*(1./15.)*s2*s_sqrt
-    c3 = (-1./12.)+(-1./6.)*s3
-    c4 = sqrt5*(2./175.)+sqrt5*(13./175.)*s3*s_sqrt
-    c5 = sqrt3*sqrt2*(-1./360.)+sqrt3*sqrt2*(-43./720.)*s4
-    c6 = sqrt7*(4./6615.)+sqrt7*(311./6615.)*s4*s_sqrt
-    c7 = sqrt2*(-1./4200.)+sqrt2*(-157./2100.)*s5
-    c8 = (2./31185.)+(2833./31185.)*s5*s_sqrt
-    return c0+u*(c1+u*(c2+u*(c3+u*(c4+u*(c5+u*(c6+u*(c7+u*c8)))))))
+def taylor_ydelta_coeffs( mode ):
+    from .mpmath import mp, mpf
+    #5th order taylor coefficients for y(x,theta=pi)-y(x,theta=0):
+    if mode == 'primary':
+        return [ mpf(0), mpf(0),
+                 mpf("2369/5775"),
+                 mpf("-108240952/91216125"),
+                 mpf("350769113251/148069498500"),
+                 mpf("-11888573193613859/2846107289025000") ]
+    if mode == 'scndgauss':
+        return [ mpf(0), mpf(0),
+                 mp.sqrt(3)*mpf("4/15"),
+                 mpf("-4/3"),
+                 mp.sqrt(5)*mpf("208/175"),
+                 mp.sqrt(3)*mp.sqrt(2)*mpf("-86/45") ]
+    if mode == 'scndlorentz':
+        return [ mpf(0), mpf(0),
+                 mpf("8/15"),
+                 mpf("-160/81"),
+                 mpf("416/81"),
+                 mpf("-4816/405") ]
+    if mode == 'scndfresnel':
+        return [ mpf(0), mpf(0),
+                 mpf("11/25"),
+                 mpf("-1208/945"),
+                 mpf("203047/79380"),
+                 mpf("-28172611/6237000") ]
+    assert False
