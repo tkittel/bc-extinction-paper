52bff3e8287ee49c2bd048f78a2791411dfaa767
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-09-25 11:50:57 +0200

    update printcode

--- src/bcextn/cli_printcode.py
+++ src/bcextn/cli_printcode.py
@@ -3,7 +3,16 @@ from . import printcode
 
 def main():
     args = sys.argv[1:]
-    if len(args)!=1 or args[0] not in printcode.languages:
-        raise SystemExit("ERROR: Please specify language (one of %s)"%(' '.join(printcode.languages)))
-    lang = args[0]
-    printcode.doit(lang)
+    def usage():
+        import os
+        bn = os.path.basename(sys.argv[0])
+        print("Usage:\n\n")
+        print('  %s (%s) [OUTFILE]\n'%(bn,'|'.join(printcode.languages)))
+        raise SystemExit(1)
+    if len(args) not in (1,2) or args[0] not in printcode.languages:
+        usage()
+    outfile = args[1] if len(args)>1 else None
+    if outfile is not None and (not outfile or not '.' in outfile or outfile.startswith('-')):
+        usage()
+    printcode.doit( lang=args[0],
+                    outfile=outfile )
