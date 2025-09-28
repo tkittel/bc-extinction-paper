811c4fbebf25525d7bc1211475b7472ef1130cb2
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-09-28 14:05:34 +0200

    ruff fixes

--- src/bcextn/cli_printcode.py
+++ src/bcextn/cli_printcode.py
@@ -12,7 +12,7 @@ def main():
     if len(args) not in (1,2) or args[0] not in printcode.languages:
         usage()
     outfile = args[1] if len(args)>1 else None
-    if outfile is not None and (not outfile or not '.' in outfile or outfile.startswith('-')):
+    if outfile is not None and (not outfile or '.' not in outfile or outfile.startswith('-')):
         usage()
     printcode.doit( lang=args[0],
                     outfile=outfile )
